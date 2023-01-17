b34f4ffca1e8905bc48f3dadb6f439bf7e691377
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-01-17 00:55:18 -0300

    refactor: trasforma bot em um modulo e coloca token em outro arquivo

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, {polling: true});
+const bot = require("./src/bot/")
 
-var items = [];
+let items = [];
 
 bot.onText(/\/add (.+)/, (msg) => {
-  const chatId = msg.chat.id;
-  let items_text = msg.text.match(/"([^"]+)"/g);
-  if(items_text){
-    items_text = items_text.map(i => i.replace(/"/g,''));
-    items.push(...items_text);
-    bot.sendMessage(chatId, `Itens "${items_text.join(',')}" adicionados com sucesso!`);
-  }else{
-    bot.sendMessage(chatId, `Nenhum item adicionado, use o formato "/add "item1", "item2", "item3"`);
-  }
-});
+	const chatId = msg.chat.id;
+	let items_text = msg.text.match(/"([^"]+)"/g);
+	if (items_text) {
+		items_text = items_text.map((i) => i.replace(/"/g, ''));
+		items.push(...items_text);
+		bot.sendMessage(chatId, `Itens "${items_text.join(',')}" adicionados com sucesso!`);
+	} else {
+		bot.sendMessage(chatId, `Nenhum item adicionado, use o formato "/add "item1", "item2", "item3"`);
+	}
+});``
 
 bot.onText(/\/roulette/, (msg) => {
-  const chatId = msg.chat.id;
-  if (items.length === 0) {
-    bot.sendMessage(chatId, 'Nenhum item adicionado, use o comando /add para adicionar itens.');
-  } else {
-    var randomItem = items[Math.floor(Math.random() * items.length)];
-    bot.sendMessage(chatId, `Resultado: ${randomItem}`);
-  }
+	const chatId = msg.chat.id;
+	if (items.length === 0) {
+		bot.sendMessage(chatId, 'Nenhum item adicionado, use o comando /add para adicionar itens.');
+	} else {
+		let randomItem = items[Math.floor(Math.random() * items.length)];
+		bot.sendMessage(chatId, `Resultado: ${randomItem}`);
+	}
 });
 
 bot.onText(/\/clear/, (msg) => {
-  const chatId = msg.chat.id;
-  items.length = 0;
-  bot.sendMessage(chatId, 'Itens limpos com sucesso!');
+	const chatId = msg.chat.id;
+	items.length = 0;
+	bot.sendMessage(chatId, 'Itens limpos com sucesso!');
 });
-
-
